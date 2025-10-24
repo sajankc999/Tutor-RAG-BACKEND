105cfbffba28db36bed34c259727b4d783a0f647
sajankc999 <kcsajan272@gmail.com>
2025-10-24 10:51:59 +0545

    feat(content): added md file extension in model

--- content/models.py
+++ content/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import FileExtensionValidator
 
 class Content(models.Model):
-    file=models.FileField(upload_to="contents/",validators=[FileExtensionValidator(allowed_extensions=['pdf','txt','docx'])])
+    file=models.FileField(upload_to="contents/",validators=[FileExtensionValidator(allowed_extensions=['pdf','txt','docx','md'])])
     topic=models.CharField(max_length=255,null=True,blank=True)
     title=models.CharField(max_length=255,null=True,blank=True)
     grade=models.CharField(max_length=255,null=True,blank=True)
